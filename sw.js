0b15d4783a3f7100aa26a2c98691245744a29657
shailendramaurya.in@gmail.com <shailendramaurya.in@gmail.com>
2023-08-29 19:49:18 +0530

    Playlist UI update and bug fixes

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js
+
+const CACHE_NAME = 'lovely-cache';
+const urlsToCache = [
+    '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
